ca7f67c6b9a648bafbe66e75d7aba38f3d8cc23b
StevenSpanos <stevenspanos7@gmail.com>
2025-03-17 17:56:22 -0500

    suicide 2.0

--- Enemy.java
+++ Enemy.java
@@ -1,5 +1,20 @@
 public class Enemy extends Entity{
+    private boolean canMove;
     public Enemy(){
         super("basic",3,"X",4,4);
+        canMove = true;
+    }
+    public Enemy(String n, int h, String c, int x, int y, boolean b){
+        super(n,h,c,x,y);
+        canMove = b;
+    }
+    public boolean canMove(){
+        return canMove;
+    }
+    public void setMove(boolean b){
+        canMove = b;
+    }
+    public String toString(){
+        return getCharacter();
     }
 }
